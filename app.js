bddb011a65181503e9bfd45d93bc43e1ae871778
yossi lerer <ylerer770@gmail.com>
2025-12-04 12:46:27 +0200

    add app (main) and finisf project

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+import readline from "readline-sync";
+import {createPlayer} from "./utils/Player.js"
+import {all} from "./riddles/allRiddels.js"
+import {measureSolveTime} from "./utils/Required.js"
+import {addSolveTime} from "./utils/addTime.js"
+import {showStats} from "./utils/printStats.js"
+import {askRiddle} from "./utils/ridelAsk.js"
+
+console.log("Welcome to the math puzzle game!!!");
+console.log("- - - - - - - - - - -  - - -");
+console.log("");
+const name = readline.question("What is your name? ");
+const player1 = createPlayer(name)
+for(let r of all){
+    let timeS = measureSolveTime(()=> askRiddle(r))
+    addSolveTime(player1, timeS)
+}
+showStats(player1)
+console.log("");
+console.log("you were great!!");
+console.log("");
+console.log("bay");
 
 
-measureSolveTime(()=> askRiddle(r1))// בשביל להפעיל נכון את הפונקציה שמודדת זמנים
+
+
+
+
+
+
---
+++ riddles/allRiddels.js
@@ -0,0 +1,7 @@
+import r1 from "./r1.js"
+import r2 from "./r2.js"
+import r3 from "./r3.js"
+import r4 from "./r4.js"
+import r5 from "./r5.js"
+
+export let all = [r1, r2,r3,r4,r5]
